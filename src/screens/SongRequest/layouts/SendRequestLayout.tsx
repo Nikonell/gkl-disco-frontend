a3809cf17ae76f10537629149ce8fc03ad070eb2
Nikonell <rozhkovn08@gmail.com>
2024-05-01 16:46:15 +0700

    feat: send loader

--- src/screens/SongRequest/layouts/SendRequestLayout.tsx
+++ src/screens/SongRequest/layouts/SendRequestLayout.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { FoundTrack, SongRequestState } from "../SongRequest";
 import { NavigateFunction, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import { SendHorizonal } from "lucide-react";
+import { Loader2, SendHorizonal } from "lucide-react";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { SongCard } from "../SongRequest";
 import { Switch } from "@/components/ui/switch";
@@ -11,40 +11,48 @@ import { Input } from "@/components/ui/input";
 import { useToast } from "@/components/ui/use-toast";
 
 interface SendRequestLayoutState {
+    loading: boolean;
     sayHello: boolean;
     helloFrom: string | null;
     helloTo: string | null;
     helloText: string | null;
 }
 
-const sendRequestHandler = async (selectedSong: FoundTrack | null, state: SendRequestLayoutState, set: Dispatch<SetStateAction<SongRequestState>>, navigate: NavigateFunction, toast: any) => {
-    const res = await fetch("/api/song_requests", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            yandex_id: selectedSong?.id,
-            say_hello: state.sayHello,
-            hello_from: state.helloFrom,
-            hello_to: state.helloTo,
-            hello_text: state.helloText,
-        }),
-    });
-    if (res.status === 409) {
-        toast({variant: "destructive", "title": "Песня уже заказана.", description: "Используйте другую песню."});
-        navigate("/");
-    } else if (!res.ok) toast({variant: "destructive", title: "Что-то пошло не так.", description: "Попробуйте ещё раз."});
-    if (!res.ok) return;
-    const data = await res.json();
-    set(p => ({...p, sentRequest: data}));
-    navigate("/result");
+const sendRequestHandler = async (selectedSong: FoundTrack | null, state: SendRequestLayoutState, setState: Dispatch<SetStateAction<SendRequestLayoutState>>, set: Dispatch<SetStateAction<SongRequestState>>, navigate: NavigateFunction, toast: any) => {
+    setState(p => ({ ...p, loading: true }));
+    try {
+        const res = await fetch("/api/song_requests", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                yandex_id: selectedSong?.id,
+                say_hello: state.sayHello,
+                hello_from: state.helloFrom,
+                hello_to: state.helloTo,
+                hello_text: state.helloText,
+            }),
+        });
+        if (res.status === 409) {
+            toast({ variant: "destructive", "title": "Песня уже заказана.", description: "Используйте другую песню." });
+            navigate("/");
+        } else if (!res.ok) toast({ variant: "destructive", title: "Что-то пошло не так.", description: "Попробуйте ещё раз." });
+        if (!res.ok) return;
+        const data = await res.json();
+        set(p => ({ ...p, sentRequest: data }));
+        navigate("/result");
+    } catch (err) {
+        toast({ variant: "destructive", title: "Что-то пошло не так.", description: "Попробуйте ещё раз." });
+    }
+    setState(p => ({ ...p, loading: false }));
 }
 
 export const SendRequestLayout = ({ selectedSong, set }: { selectedSong: FoundTrack | null, set: Dispatch<SetStateAction<SongRequestState>> }) => {
     const navigate = useNavigate();
     const { toast } = useToast();
     const [state, setState] = useState<SendRequestLayoutState>({
+        loading: false,
         sayHello: false,
         helloFrom: null,
         helloTo: null,
@@ -52,8 +60,8 @@ export const SendRequestLayout = ({ selectedSong, set }: { selectedSong: FoundTr
     });
 
     const switchHello = () => {
-        if (!state.sayHello) setState(p => ({...p, sayHello: true}));
-        else setState(p => ({...p, sayHello: false, helloFrom: null, helloTo: null, helloText: null}));
+        if (!state.sayHello) setState(p => ({ ...p, sayHello: true }));
+        else setState(p => ({ ...p, sayHello: false, helloFrom: null, helloTo: null, helloText: null }));
     }
 
     useEffect(() => {
@@ -67,31 +75,36 @@ export const SendRequestLayout = ({ selectedSong, set }: { selectedSong: FoundTr
                 <CardDescription>Укажите дополнительные данные</CardDescription>
             </CardHeader>
             <CardContent className="flex flex-col gap-4 pb-4">
-                {selectedSong != null && <SongCard track={selectedSong as FoundTrack} selected={false} onClick={() => {}} />}
+                {selectedSong != null && <SongCard track={selectedSong as FoundTrack} selected={false} onClick={() => { }} />}
                 <div className="flex flex-row gap-2 items-center">
                     <Switch id="say-hello" checked={state.sayHello} onClick={switchHello} />
                     <Label htmlFor="say-hello">Передать привет</Label>
                 </div>
                 <div className="flex flex-col gap-1.5">
                     <Label htmlFor="hello-from">От кого</Label>
-                    <Input type="text" id="hello-from" placeholder="От кого" disabled={!state.sayHello} 
-                        value={state.helloFrom || ""} onChange={e => setState(p => ({...p, helloFrom: e.target.value}))}/>
+                    <Input type="text" id="hello-from" placeholder="От кого" disabled={!state.sayHello}
+                        value={state.helloFrom || ""} onChange={e => setState(p => ({ ...p, helloFrom: e.target.value }))} />
                 </div>
                 <div className="flex flex-col gap-1.5">
                     <Label htmlFor="hello-to">Кому</Label>
                     <Input type="text" id="hello-to" placeholder="Кому" disabled={!state.sayHello}
-                        value={state.helloTo || ""} onChange={e => setState(p => ({...p, helloTo: e.target.value}))}/>
+                        value={state.helloTo || ""} onChange={e => setState(p => ({ ...p, helloTo: e.target.value }))} />
                 </div>
                 <div className="flex flex-col gap-1.5">
                     <Label htmlFor="hello-text">Комментарий</Label>
-                    <Input type="text" id="hello-text" placeholder="Комментарий" disabled={!state.sayHello} 
-                        value={state.helloText || ""} onChange={e => setState(p => ({...p, helloText: e.target.value}))}/>
+                    <Input type="text" id="hello-text" placeholder="Комментарий" disabled={!state.sayHello}
+                        value={state.helloText || ""} onChange={e => setState(p => ({ ...p, helloText: e.target.value }))} />
                 </div>
             </CardContent>
             <CardFooter>
-                <Button className="w-full" onClick={() => sendRequestHandler(selectedSong, state, set, navigate, toast)}>
-                    Отправить
-                    <SendHorizonal className="w-4 h-4 ml-2" />
+                <Button className="w-full" disabled={state.loading} onClick={() => sendRequestHandler(selectedSong, state, setState, set, navigate, toast)}>
+                    {state.loading ? <>
+                        Отправка
+                        <Loader2 className="ml-2 h-4 w-4 animate-spin" />
+                    </> : <>
+                        Отправить
+                        <SendHorizonal className="w-4 h-4 ml-2" />
+                    </>}
                 </Button>
             </CardFooter>
         </Card>
